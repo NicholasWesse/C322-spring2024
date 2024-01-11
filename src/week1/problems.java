14c1018ea3f8bac09009719d4f26de4d9994a5b1
nwesseli <nwesseli@iu.edu>
2024-01-11 12:42:59 -0500

    Lab1

--- src/week1/problems.java
+++ src/week1/problems.java
@@ -1,5 +1,7 @@
 package week1;
 
+import java.util.LinkedList;
+
 public class problems {
 
     public static void main(String[] args) {
@@ -22,5 +24,76 @@ public class problems {
         }
         return result;
     }
+
+    public static boolean problem2(int x){
+        //Palindrome
+        int hold = x;
+        int reversed = 0;
+        int holder = 0;
+
+        while(x > 0){
+            holder = x % 10;
+            reversed = (reversed * 10) + holder;
+            x = x/10;
+        }
+
+        if(reversed == hold){return true;}
+        else return false;
+
+
+    }
+
+
+    public static String problem3(String[] strings){
+            //prefix checker
+        String output = "";
+        int shortest =255;
+
+        //find shortest word length
+        for(int i=0; i< strings.length; ++i){
+            if(strings[i].length() <  shortest){
+                shortest = strings[i].length();
+            }
+        }
+        boolean works = true;
+        for(int j=0; j < shortest; ++j){
+            char prefix = strings[0].charAt(j);
+            for(int k=1; k < strings.length; ++k){
+                if(strings[k].charAt(j) != prefix) {
+                    works = false;
+                }
+            }
+            if(!works){break;}
+            else{
+                output = output + prefix;
+            }
+        }
+        return output;
+    }
+
+
+    public static int problem4(String roman){
+        int i= 0;
+        int total = 0;
+        while(!roman.isEmpty()){
+            if(roman.charAt(i) == 'I'){
+                if(roman.charAt(i+1) == 'V'){
+                    total += 4;
+                }
+                total += 1;
+            }
+
+
+
+
+
+            roman.substring(1, roman.length());
+        }
+    }
+    public static boolean problem5(String x){
+        return true;
+    }
+    public static LinkedList<Integer> problem6(LinkedList<Integer> x, LinkedList<Integer> x2){}
+    public static int[] problem7(int[] x){}
 
 }
--- src/week1/testDrive.java
+++ src/week1/testDrive.java
@@ -1,5 +1,7 @@
 package week1;
 
+import java.util.LinkedList;
+
 public class testDrive {
 
     public static void main(String[] args) {
@@ -13,8 +15,6 @@ public class testDrive {
 
         boolean firstTest = true;
 
-        System.out.println(problems.problemOne(nums, target)[0]);
-        System.out.println(problems.problemOne(nums, target)[1]);
         for(int i=0; i<2; ++i){
             if(problems.problemOne(nums, target)[0] != expected[0]){
                 firstTest = false;
@@ -23,7 +23,6 @@ public class testDrive {
                 firstTest = false;
             }
         }
-        System.out.println("First test should output True and outputs " + firstTest);
         if(firstTest){ ++testsPassed;}
 
         ////////////////////////////////////////////////////////////////////
@@ -43,7 +42,6 @@ public class testDrive {
                 firstTest2 = false;
             }
         }
-        System.out.println("Second test should output True and outputs " + firstTest2);
 
         if(firstTest2){ ++testsPassed;}
 
@@ -64,10 +62,124 @@ public class testDrive {
                 firstTest3 = false;
             }
         }
-        System.out.println("Third test should output True and outputs " + firstTest3);
 
         if(firstTest3){ ++testsPassed;}
 
+        System.out.println(testsPassed + " out of 3 Passed");
+
+
+
+        ///////////////////////////////////////////////////
+        //Problem 2
+        int problem2Passed = 0;
+
+        int x1 = 121;
+        int x2 = 2112;
+        int x3 = 321;
+
+       if(problems.problem2(x1)){++problem2Passed;}
+        if(problems.problem2(x2)){++problem2Passed;}
+        if(!problems.problem2(x3)){++problem2Passed;}
+
+        System.out.println(problem2Passed + " out of 3 Passed");
+
+
+        /////////////////////////////////////////////////////////
+        //Problem 3
+
+        //Longest Prefix
+
+        int problem3Passed = 0;
+
+        String[] st1 = {"Flower", "Flow", "Flood"};
+        String[] st2 = {"Po", "Ti", "Ur"};
+        String[] st3 = {"dog", "racecar", "car"};
+
+        if(problems.problem3(st1).equals("Flo")){++problem3Passed;}
+        if(problems.problem3(st2).equals("")){++problem3Passed;}
+        if(problems.problem3(st3).equals("")){++problem3Passed;}
+
+        System.out.println(problem3Passed + " out of 3 Passed");
+
+
+
+        ////////////////////////////////////////////////////
+        //Problem 4
+
+        //Roman Numerals
+
+        int problem4Passed = 0;
+        String r1 = "IV";
+        String r2 =  "LXI";
+        String r3 = "MVI";
+
+
+        if(problems.problem4(r1) == 4){++problem4Passed;}
+        if(problems.problem4(r2) == 61){++problem4Passed;}
+        if(problems.problem4(r3) == 1006){++problem4Passed;}
+
+
+
+        /////////////////////////////////////////////////////////
+        //Problem 5
+
+        //Bracket Closing
+
+        int problem5Passed = 0;
+        String b1 = "{{}}";
+        String b2 = "({})";
+        String b3 = "(({{})";
+
+        if(problems.problem5(b1)){++problem5Passed;}
+        if(problems.problem5(b2)){++problem5Passed;}
+        if(!problems.problem5(b3)){++problem5Passed;}
+
+
+
+        //////////////////////////////////////////////////////////
+        //Problem 6
+
+        // Merge Linked Lists
+
+
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer> l2 = new LinkedList<>();
+        LinkedList<Integer> l3 = new LinkedList<>();
+        LinkedList<Integer> l4 = new LinkedList<>();
+
+        l1.add(1);
+        l1.add(2);
+        l1.add(4);
+
+        l2.add(2);
+        l2.add(5);
+
+        l3.add(5);
+        l3.add(2);
+        l3.add(2);
+
+        l4.add(6);
+        l4.add(4);
+        l4.add(9);
+
+
+
+
+
+        ////////////////////////////////////////////////////////
+        //Problem 7
+
+
+        //Increment by 1
+
+
+
+        int[] s1 = {1,2,3,4};
+        int[] s2 = {3,4,5};
+        int[] s3 = {9};
+
+
+        int[] hold1 = problems.problem7(s1);
 
 
 
